1cfde646cc12f75b345db1de8ab02105cbe3c11c
lpearf-pixel <lpearf@gmail.com>
2026-07-22 12:35:29 +0800

    build: export private knowledge index

---
+++ src/project_agent_controller/knowledge/__init__.py
@@ -0,0 +1,29 @@
+from project_agent_controller.knowledge.index import (
+    IndexStats,
+    KnowledgeIndex,
+    KnowledgeMatch,
+    KnowledgeMatches,
+    QuarantineItem,
+)
+from project_agent_controller.knowledge.models import (
+    Applicability,
+    KnownProblem,
+    Lesson,
+    LessonStatus,
+    PromptMetadata,
+    PromptStatus,
+)
+
+__all__ = [
+    "Applicability",
+    "IndexStats",
+    "KnowledgeIndex",
+    "KnowledgeMatch",
+    "KnowledgeMatches",
+    "KnownProblem",
+    "Lesson",
+    "LessonStatus",
+    "PromptMetadata",
+    "PromptStatus",
+    "QuarantineItem",
+]
