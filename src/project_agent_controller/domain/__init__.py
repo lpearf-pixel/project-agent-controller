d29ff1f9bbd459507d7c83455742f6cd85bc4f69
lpearf-pixel <lpearf@gmail.com>
2026-07-22 12:09:00 +0800

    build: export domain models

---
+++ src/project_agent_controller/domain/__init__.py
@@ -0,0 +1,15 @@
+from project_agent_controller.domain.models import (
+    EventRecord,
+    FileSourceConfig,
+    ProjectConfig,
+    ProjectsConfig,
+    Severity,
+)
+
+__all__ = [
+    "EventRecord",
+    "FileSourceConfig",
+    "ProjectConfig",
+    "ProjectsConfig",
+    "Severity",
+]
